0b2fc3d5bf452ac0fffcf6a2df9fbb2f96abb3b8
Alex Arvedal <Alex Arvedal>
2016-10-26 15:52:31 +0200

    Git test

---
+++ GitTest.java
@@ -0,0 +1,20 @@
+package v43;
+
+import java.util.Scanner;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+        int age;
+
+        
+
+        System.out.print("Enter age: ");
+
+        age = input.nextInt();
+        System.out.println("You are:" + age + " years old");
+	}
+
+}
